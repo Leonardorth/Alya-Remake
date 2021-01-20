57c820fae53bd5f50964dbb5aa940e26fa8b8207
L3ON4RDO <matthawes777@gmail.com>
2021-01-20 21:11:35 +0100

    Fourth Level with Cutscene completed + fixes
    Finished the Alpha version of the game ending on a cutscene that takes the player back to the main menu

---
+++ Alya Remake/Assets/CutsceneLoadMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneLoadMainMenu : MonoBehaviour
+{
+    public GameObject player;
+
+    public void TimelineLoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void DisablePlayer()
+    {
+        player.SetActive(false);
+    }
+
+}
